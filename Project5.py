93f7a8762a784514517cce75a490798f3c5c443f
Jack <msgonsalves@wpi.edu>
2018-09-28 15:36:41 -0400

    is_final_permissible functions done for everything but weightLimit class

--- Project5.py
+++ Project5.py
@@ -4,7 +4,7 @@ class UnaryInclusive:
         self.item = item
         self.bags = bags
 
-    def is_permissible(self, solution):
+    def is_final_permissible(self, solution):
 
         for a_bag in solution:
             if self.item in a_bag:
@@ -13,7 +13,7 @@ class UnaryInclusive:
                 else:
                     return False
 
-        return True
+        return -1
 
 
 class FittingLimit:
@@ -22,12 +22,15 @@ class FittingLimit:
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
 
-    def is_permissible(self, solution):
+    def is_final_permissible(self, solution):
 
-        # for a_bag in solution:
-        #     if  len(solution) < self.lower_bound +1 :
-        pass
+        is_valid = True
+        for a_bag in solution:
+            if not self.lower_bound <= len(a_bag) - 1 <= self.upper_bound:
+                is_valid = False
+                break
 
+        return is_valid
 
 
 class UnaryExclusive:
@@ -36,8 +39,16 @@ class UnaryExclusive:
         self.item = item
         self.bags = bags
 
-    def is_permissible(self):
-        pass
+    def is_final_permissible(self, solution):
+
+        for a_bag in solution:
+            if self.item in a_bag:
+                if a_bag[0] in self.bags:
+                    return False
+                else:
+                    return True
+
+        return -1
 
 
 class BinaryEquals:
@@ -45,8 +56,16 @@ class BinaryEquals:
     def __init__(self, items):
         self.items = items
 
-    def is_permissible(self):
-        pass
+    def is_final_permissible(self, solution):
+
+        for a_bag in solution:
+            if self.items[0] in a_bag:
+                if self.items[1] in a_bag:
+                    return True
+                else:
+                    return False
+
+        return -1
 
 
 class BinaryNotEquals:
@@ -54,8 +73,15 @@ class BinaryNotEquals:
     def __init__(self, items):
         self.items = items
 
-    def is_permissible(self):
-        pass
+    def is_final_permissible(self, solution):
+        for a_bag in solution:
+            if self.items[0] in a_bag:
+                if self.items[1] in a_bag:
+                    return False
+                else:
+                    return True
+
+        return -1
 
 
 class MutualInclusive:
@@ -63,6 +89,39 @@ class MutualInclusive:
     def __init__(self, items, bags):
         self.items = items
         self.bags = bags
+
+    def is_final_permissible(self, solution):
+
+        for a_f_bag in solution:
+            for i in range(0, 1):
+                if self.items[i] in a_f_bag:
+                    if i == 1:
+                        opp_i = 0
+                    else:
+                        opp_i = 1
+
+                    for j in range(0, 1):
+                        if self.bags[j] == a_f_bag[0]:
+                            if j == 1:
+                                opp_j = 0
+                            else:
+                                opp_j = 1
+
+                                for a_s_bas in solution:
+                                    if self.items[opp_i] in a_s_bas:
+                                        if self.bags[opp_j] == a_s_bas[0]:
+                                            return True
+                                        else:
+                                            return False
+
+        return True
+
+
+class WeightLimit:
+
+    def __init__(self, bag, weight):
+        self.bag = bag
+        self.weight = weight
 
     def is_permissible(self):
         pass
